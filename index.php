21fdb8cc73c6c557c146407d7044e8a821a8c568
Evrim <evrimcabuk@gmail.com>
2010-12-18 21:32:39 +0200

    Yeni özellikler eklendi ;)

--- index.php
+++ index.php
@@ -13,6 +13,25 @@ and open the template in the editor.
 echo "Deneme ;)";
 echo "YahoO!";
 echo "YahoO2!";
+
+<?php
+if ($_POST) {
+    echo '<pre>';
+    echo htmlspecialchars(print_r($_POST, true));
+    echo '</pre>';
+}
+?>
+<form action="" method="post">
+    Adƒ±:  <input type="text" name="personal[isim]" /><br />
+    Eposta: <input type="text" name="personal[eposta]" /><br />
+    Bira: <br />
+    <select multiple name="bira[]">
+        <option value="efes">Efes</option>
+        <option value="tuborg">Tuborg</option>
+        <option value="venus">Ven√ºs</option>
+    </select><br />
+    <input type="submit" value="G√∂nder!" />
+</form>
         ?>
     </body>
 </html>
